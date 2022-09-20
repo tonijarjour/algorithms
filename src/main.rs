aa3887294404586f856ad07f131b3519d775b9b6
Toni Jarjour <39845673+tonijarjour@users.noreply.github.com>
2022-09-20 16:00:16 -0400

    complete insert and remove methods

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,13 @@ fn main() {
     let mut list = singly_linked::List::new();
     list.push(5u8);
     list.push(6);
+    list.push(7);
+    list.push(8);
+    list.push(9);
+    list.deq();
+    list.pop();
+    list.deq();
+    list.pop();
 
     println!("{:#?}", list);
 }
--- src/singly_linked.rs
+++ src/singly_linked.rs
@@ -1,5 +1,5 @@
-use std::rc::Rc;
 use std::cell::RefCell;
+use std::rc::Rc;
 
 #[derive(Debug)]
 struct Node<T: std::fmt::Debug> {
@@ -32,15 +32,29 @@ impl<T: std::fmt::Debug> List<T> {
     pub fn insert(&mut self, index: usize, value: T) {
         if index == 0 {
             if let None = self.head {
+                // set head to a new node
                 self.head = Some(Rc::new(RefCell::new(Node::new(value, None))));
+                // tail references the same node
                 self.tail = Some(Rc::clone(self.head.as_ref().unwrap()));
             } else {
-                let old_head = std::mem::replace(&mut self.head, None);
-                self.head = Some(Rc::new(RefCell::new(Node::new(value, old_head))));
+                // retrieve the old head, replacing it with a new node
+                let old_head = std::mem::replace(
+                    &mut self.head,
+                    Some(Rc::new(RefCell::new(Node::new(value, None)))),
+                );
+                // have the new head point to the old head
+                self.head.as_ref().unwrap().borrow_mut().next = old_head;
             }
         } else if index == self.size {
-            let node = Some(Rc::new(RefCell::new(Node::new(value, None))));
-            let old_tail = std::mem::replace(&mut self.tail, None);
+            // set the old tail to point to a new node
+            self.tail.as_ref().unwrap().borrow_mut().next =
+                Some(Rc::new(RefCell::new(Node::new(value, None))));
+            // create a another reference to that node
+            let new_tail = Some(Rc::clone(
+                &self.tail.as_ref().unwrap().borrow().next.as_ref().unwrap(),
+            ));
+            // set the tail to that reference
+            self.tail = new_tail;
         }
         self.size += 1;
     }
@@ -53,11 +67,42 @@ impl<T: std::fmt::Debug> List<T> {
         Self::insert(self, 0, value);
     }
 
-    pub fn remove(&mut self, index: usize) {}
+    pub fn remove(&mut self, index: usize) -> bool {
+        if self.size == 0 || index >= self.size {
+            return false;
+        }
 
-    pub fn pop(&mut self) {}
+        if index == 0 {
+            // take the current head's next and set the head to it
+            let new_head = std::mem::replace(
+                &mut self.head.as_ref().unwrap().borrow_mut().next,
+                None,
+            );
+            self.head = new_head;
+        } else if index == self.size - 1 {
+            // new reference to the first element in the list
+            let mut current = Rc::clone(self.head.as_ref().unwrap());
+            // find the second to last element
+            for n in 0..self.size - 2 {
+                let next = Rc::clone(current.borrow().next.as_ref().unwrap());
+                current = next;
+            }
+            // set its next value to none
+            current.borrow_mut().next = None;
+            // set it as the tail
+            self.tail = Some(current);
+        }
+        self.size -= 1;
+        true
+    }
 
-    pub fn deq(&mut self) {}
+    pub fn pop(&mut self) -> bool {
+        Self::remove(self, self.size - 1)
+    }
+
+    pub fn deq(&mut self) -> bool {
+        Self::remove(self, 0)
+    }
 
     pub fn position(&self, value: T) {}
 
